f80e08ee1523bd32e237f65c9f9fb929f7c0b81a
Girma Nigusse <xgirma@gmail.com>
2019-09-21 21:31:22 -0400

    class

--- test/class/class.test.js
+++ test/class/class.test.js
@@ -1,14 +1,21 @@
 import Rectangle from "../../src/class";
 
 describe("Class", () => {
+  let r;
+  beforeEach(() => {
+    r = new Rectangle();
+  });
+
   it("should instantiate", () => {
-    const r = new Rectangle();
     expect(!!r).toEqual(true, "instance exist");
     expect(r instanceof Rectangle).toBe(true, "instance exist");
   });
 
   it("should have constructor", () => {
-    const r = new Rectangle();
     expect(r.constructor.name).toEqual("Rectangle", "constructor exists");
+  });
+
+  it("should have a method", () => {
+    expect(typeof r.calcArea).toEqual("function", "method exists");
   });
 });
